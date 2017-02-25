3876214831c833b503608d85ed91a3871a849179
Issei Naruta <naruta@cookpad.com>
2017-02-25 20:20:23 +0900

    Add gunosy example

--- mirakui_scrapy/items.py
+++ mirakui_scrapy/items.py
@@ -11,4 +11,6 @@ import scrapy
 class MirakuiScrapyItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    url = scrapy.Field()
+    subcategory = scrapy.Field()
